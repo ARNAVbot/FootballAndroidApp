754473d46f97bd598792b934b4697a53872e0ac6
arnav agarwal <arnavagarwal@urbancompany.com>
2021-07-24 17:26:00 +0530

    added submodule

--- shared/src/commonMain/kotlin/com/arnav/shared/Greeting.kt
+++
@@ -1,7 +0,0 @@
-package com.arnav.shared
-
-class Greeting {
-    fun greeting(): String {
-        return "Hello, ${Platform().platform}!"
-    }
-}
--- shared/src/commonMain/kotlin/com/arnav/shared/Platform.kt
+++
@@ -1,5 +0,0 @@
-package com.arnav.shared
-
-expect class Platform() {
-    val platform: String
-}
